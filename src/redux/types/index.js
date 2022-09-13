a1b2dcd42f4322d10250161f9f4c7af6d8dbc558
genaroibc <bonavitaismael@gmail.com>
2022-09-13 17:57:03 -0300

    add crud types

--- src/redux/types/index.js
+++ src/redux/types/index.js
@@ -3,3 +3,9 @@ export const INCREMENT_FIVE = "INCREMENT_FIVE";
 export const DECREMENT_ONE = "DEREMENT_ONE";
 export const DECREMENT_FIVE = "DECREMENT_FIVE";
 export const RESET = "RESET";
+
+// crud
+export const GET_ALL_DATA = "GET_ALL_DATA";
+export const DELETE_ONE = "DELETE_ONE";
+export const UPDATE_ONE = "UPDATE_ONE";
+export const CREATE_ONE = "CREATE_ONE";
