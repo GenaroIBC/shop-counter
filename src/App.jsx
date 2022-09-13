ae6273a28dec9be4357be845956f0020626aded3
genaroibc <bonavitaismael@gmail.com>
2022-09-13 17:18:53 -0300

    feat: implement counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Provider } from "react-redux";
 import "./App.css";
+import { Counter } from "./components/Counter";
+import { store } from "./redux/store";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Provider store={store}>
+      <h1>Hello</h1>
+      <Counter />
+    </Provider>
+  );
 }
 
 export default App;
